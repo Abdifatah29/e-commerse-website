54c0b4c07b8164848fe53d5e4cb8e04da9255dee
Abdifatah29 <abdi.hassannoor01@gmail.com>
2019-10-25 10:19:21 +0200

    version 2

--- js/main.js
+++ js/main.js
@@ -24,3 +24,22 @@ $('.slider-two')
         slidesToScroll: 1,
         autoplaySpeed: 3000
     });
+
+    // contact page
+jQuery(function ($) {
+    function init_map1() {
+        var myLocation = new google.maps.LatLng(-33.918861, 18.423300);
+        var mapOptions = {
+            center: myLocation,
+            zoom: 16
+        };
+        var marker = new google.maps.Marker({
+            position: myLocation,
+            title: "Property Location"
+        });
+        var map = new google.maps.Map(document.getElementById("map1"),
+            mapOptions);
+        marker.setMap(map);
+    }
+    init_map1();
+});
